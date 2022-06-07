6ba71756449d47d5629a8453c8ee3b613d0d8cd8
Yun Chen <1183728554@qq.com>
2022-06-07 16:04:13 +0800

    Finished!

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
 // TODO write your code here...
-
+name = name.toUpperCase()
+console.log(` '${name}' `)
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
-
+sentence = sentence.split(' ').map(s => {
+    return s.replace(s.charAt(0), s.charAt(0).toUpperCase())
+}).reduce((a, b) => {
+    return a + " " + b
+})
+console.log(sentence)
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+money = money.match(/\d+/g).join('')
+console.log(money)
